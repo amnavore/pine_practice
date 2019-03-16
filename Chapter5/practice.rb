79fbbdd289dc2c75a8285585e1c576edc3bc59d5
Amna Bhatti <abhatti1@hawk.iit.edu>
2019-03-16 04:47:47 -0500

    Create program for bigger, better number

--- Chapter5/practice.rb
+++ Chapter5/practice.rb
@@ -6,3 +6,9 @@ middlename = gets.chomp
 puts 'And what is your last name?'
 lastname = gets.chomp
 puts 'Hello, ' + firstname + ' ' + middlename + ' ' + lastname + ', it\'s nice to meet you!'
+
+#Bigger, better favorite number
+puts 'Hi there! What\'s your favorite number?'
+favnum = gets.chomp.to_i
+bignum = favnum + 1
+puts 'That\'s your favorite number? I think  '+bignum.to_s+' is even better!'
