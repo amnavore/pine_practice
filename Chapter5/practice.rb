5e605b56909253d2a1ad1cfd2fbce3fe5fd51865
Amna Bhatti <abhatti1@hawk.iit.edu>
2019-03-16 04:32:38 -0500

    Create program for full name greeting

---
+++ Chapter5/practice.rb
@@ -0,0 +1,8 @@
+#Full name greeting
+puts 'Hello there! What is your first name?'
+firstname = gets.chomp
+puts 'Hello, ' + firstname + '! What is your middle name?'
+middlename = gets.chomp
+puts 'And what is your last name?'
+lastname = gets.chomp
+puts 'Hello, ' + firstname + ' ' + middlename + ' ' + lastname + ', it\'s nice to meet you!'
