b3f7f200a6ba519b5526a9bce2ebf721f3608836
Amna Bhatti <abhatti1@hawk.iit.edu>
2019-03-16 05:02:54 -0500

    Practicing length method

--- Chapter6/moremethods.rb
+++ Chapter6/moremethods.rb
@@ -15,3 +15,9 @@ puts self
   puts var1
   puts var2
   puts var3
+
+  #Length method
+  puts 'What is your full name?'
+  name = gets.chomp
+  puts 'Did you know there are ' + name.length.to_s + ' characters'
+  puts 'in your name, ' + name + '?'
