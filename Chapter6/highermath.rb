08cdcc8dcd828271f1504f0f0fae5e2a861ea77f
Amna Bhatti <abhatti1@hawk.iit.edu>
2019-03-17 02:23:44 -0500

    Working with srand and Math object

--- Chapter6/highermath.rb
+++ Chapter6/highermath.rb
@@ -18,9 +18,11 @@
 puts rand
 puts rand
 puts rand
+#(Random numbers from 0-99)
 puts(rand(100))
 puts(rand(100))
 puts(rand(100))
+#(Can only return 0)
 puts(rand(1))
 puts(rand(1))
 puts(rand(1))
@@ -28,3 +30,22 @@ puts(rand(999999999999999999999999999))
 puts 'The weatherman says there is a'
 puts rand(101).to_s + '% chance of rain,'
 puts 'but you can never trust a weatherman.'
+#Returning the same random numbers
+srand 1976
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts ''
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+
+#The Math Object
+puts(Math::PI)
+puts(Math::E)
+puts(Math.cos(Math::PI/3))
+puts(Math.tan(Math::PI/4))
+puts(Math.log(Math::E**2))
+puts((1+Math.sqrt(5))/2)
