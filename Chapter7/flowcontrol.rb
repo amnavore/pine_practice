74cd5dc3c41a886f9c6fdc93e9127162495b0243
Amna Bhatti <abhatti1@hawk.iit.edu>
2019-03-17 02:47:34 -0500

    Practicing with branches on branches

--- Chapter7/flowcontrol.rb
+++ Chapter7/flowcontrol.rb
@@ -28,3 +28,22 @@ else
   puts 'Your future is...oh my! Look at the time!'
   puts 'I really have to go, sorry!'
 end
+
+#Branches with branches
+puts 'Hello, and welcome to seventh grade English.'
+puts 'My name is Ms. Gabbard. And your name is...?'
+name = gets.chomp
+
+if name == name.capitalize
+  puts 'Please take a seat, ' + name + '.'
+else
+  puts name + 'You mean ' + name.capitalize + ',right?'
+  puts 'Don\'t you even know how to spell your name??'
+  reply = gets.chomp
+
+  if reply.downcase == 'yes'
+    puts 'Hmmph! Well, sit down!'
+  else
+    puts 'GET OUT!'
+  end
+end
