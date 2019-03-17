94233b80ade7fe89d7ed72d45eff30c8e1ae09c0
Amna Bhatti <abhatti1@hawk.iit.edu>
2019-03-17 02:33:18 -0500

    Practicing comparison methods

---
+++ Chapter7/flowcontrol.rb
@@ -0,0 +1,13 @@
+#Comparison methods
+puts 1 > 2
+puts 1 < 2
+puts 5 >= 5
+puts 5 <= 4
+puts 1 == 1 #Are these equal?
+puts 2 != 1 #Are these different?
+#Comparing strings
+puts 'cat' < 'dog'
+puts 'bug lady' < 'Xander'
+puts 'bug lady'.downcase < 'Xander'.downcase
+puts 2 < 10
+puts '2' < '10' #Returns false because 0 character comes after 2
