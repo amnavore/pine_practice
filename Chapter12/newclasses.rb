4867cc38b6cac6376d78c7aec12c5491948bd342
Amna Bhatti <abhatti1@hawk.iit.edu>
2019-03-18 03:31:41 -0500

    More about strings

--- Chapter12/newclasses.rb
+++ Chapter12/newclasses.rb
@@ -38,3 +38,28 @@ end
 puts
 
 #Stringy superpowers
+da_man = 'Mr. T'
+big_T = da_man[4] #Picks the character in 4th position
+puts big_T
+
+puts 'Hello. My name is Julian.'
+puts "I'm extremely perceptive."
+puts "What's your name?"
+
+name = gets.chomp
+puts "Hi, #{name}."
+
+if name[0] == 'A'
+  puts 'You have excellent taste in footwear.'
+  puts 'I can just tell.'
+end
+
+prof = 'We tore the universe a new space-hole, alright!'
+puts prof[12, 8]
+puts prof[12..19]
+puts
+def is_avi? filename
+  filename.downcase[-4..-1] == 'avi'
+end
+puts is_avi?('DANCEMONKEYBOY.AVI')
+puts is_avi?('toilet_paper_fiasco.jpg')
