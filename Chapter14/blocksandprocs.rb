8430165adcbbd66e5c23d595b06224d025ee01ab
Amna Bhatti <abhatti1@hawk.iit.edu>
2019-03-18 04:22:16 -0500

    Working with methods that return procs

--- Chapter14/blocksandprocs.rb
+++ Chapter14/blocksandprocs.rb
@@ -82,3 +82,24 @@ end
 puts do_until_false([5], build_array_of_squares).inspect #Like to_s, but shows code for building object
 yum = 'lemonade with a hint of orange blossom water'
 puts do_until_false(yum, always_false)
+
+#Methods that return procs
+def compose proc1, proc2
+  Proc.new do |x|
+    proc2.call(proc1.call(x))
+  end
+end
+
+square_it = Proc.new do |x|
+  x * x
+end
+
+double_it = Proc.new do |x|
+  x + x
+end
+
+double_then_square = compose double_it, square_it
+square_then_double = compose square_it, double_it
+
+puts double_then_square.call(5)
+puts square_then_double.call(5)
