536b8367a129c88b9089c771dd0a10dc2a80c4f9
Amna Bhatti <abhatti1@hawk.iit.edu>
2019-03-18 01:45:30 -0500

    Double quoted strings and interpolation

--- Chapter11/saving.rb
+++ Chapter11/saving.rb
@@ -24,3 +24,25 @@ read_string = File.read filename
 read_array = YAML::load read_string
 puts(read_string == test_string)
 puts(read_array == test_array)
+
+#Double-Quoted Strings
+buffyquote1 = '\'Kiss rocks\'?
+              Why would anyone want to kiss...
+              Oh, wait. I get it.'
+buffyquote2 = "'Kiss rocks'?\n" +
+              "why would anyone want to kiss...\n"
+              "Oh, wait. I get it."
+puts buffyquote1
+puts
+puts(buffyquote1 == buffyquote2)
+
+puts "3...\n2...\n1\...\nHAPPY NEW YEAR!"
+puts '3...\n2...\n1...\nHAPPY NEW YEAR!' #Newline character doesn't work for single quotes
+
+puts 'single (\') and double (") quotes'
+puts "single (') and double (\") quotes"
+
+#Interpolation
+name = 'Luke'
+zip = 90210
+puts "Name = #{name}, Zipcode = #{zip}"
